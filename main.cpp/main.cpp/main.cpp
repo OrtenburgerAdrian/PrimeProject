9d9bfacdad75dad0a18925722c0cf9c75d004ff5
LocalWinX <LocalWinX@LOCALWINX>
2017-08-31 12:02:32 +0100

    ...

--- main.cpp/main.cpp/main.cpp
+++ main.cpp/main.cpp/main.cpp
@@ -1,4 +1,5 @@
-//Adrian und Benni
+//Adrian und Bennjamin
+//verbose 1-argument, weiter lreuchen
 #pragma once
 #include "LinkedList.hpp"
 #include "SingelCore.hpp"
@@ -10,34 +11,35 @@
 #include "Communicator.hpp"
 #endif
 
-
 int numberOfWorker = 0;						//Anzah der benoetigten Worker-Threads, wenn numberOfWorker=0, dann wird trotzdem einer gestartet.
 unsigned long long maxPrime = 1;			//Die groeszte bis jetzt gespeicherte Primzahl, ist für jeden Client-PC unterschiedlich.
 LinkedList primesList;						//Repraesentiert die erste Node.
 LinkedList *head = &primesList;				//Adresse der ersten Node.
 LinkedList* PrimeListLast = &primesList;	//Adresse der letzten Node.
-
+bool moreLog = false;						//Um alle gefundenen Primzahlen mitzuschreiben, braucht viel CPU-Time und Speicherplatz. 
 
 int main(int argc, char *argv[]) {
 
-#if defined _WIN64
-	numberOfWorker = 8;
-#elif __linux__
-	numberOfWorker = sysconf(_SC_NPROCESSORS_ONLN) - 1;   // liest die Anzahl der CPU-Core's aus, -1 damit der Comunicator-Thread im besten fall einen eigenen Core bekommen kann.
-#endif
-	//numberOfWorker = 1; //Uncomment to manually set number of working threads.
-
+	std::string arg1 = "";
+	int i = 1;
+	std::string arg1 = argv[i];
+	if (arg1 == "verbose") { moreLog = true; i++; } //Wenn "verbose" als parameter uebergeben wird, wird moreLog auf 
 
 #if defined __linux__
-	if (argc <= 1) {
+	numberOfWorker = sysconf(_SC_NPROCESSORS_ONLN) - 1;   // liest die Anzahl der CPU-Core's aus, -1 damit der Comunicator-Thread im besten fall einen eigenen Core bekommen kann.
+#endif
+														  //numberOfWorker = 1; //Uncomment to manually set number of working threads.
+
+#if defined __linux__
+	if (argc <= 1 || arg1 == "verbose" && argc <= 2) {
 		std::cout << " Inadequate transfer parameters" << std::endl;
 	}
-	else if (argc > 3) {
+	else if (argc > 3 || arg1 == "verbose" && argc > 4) {
 		std::cout << " Too many transfer parameters" << std::endl;
 	}
 
 
-	std::string arg1 = argv[1];
+	std::string arg1 = argv[i];
 	if (arg1 == "multicore") {
 		MultiCore::run();
 		std::cout << " run multicore.run();" << std::endl;
@@ -50,7 +52,7 @@ int main(int argc, char *argv[]) {
 	}
 	else if (arg1 == "observer") {
 		if (argc == 3) {
-			Observer::run(atoi(argv[2]));
+			Observer::run(atoi(argv[i + 1]));
 		}
 		else {
 			Observer::run();
@@ -59,7 +61,7 @@ int main(int argc, char *argv[]) {
 	else if (arg1 == "communicator") {
 		Worker::start();
 		if (argc == 3) {
-			Communicator::run(argv[2]);
+			Communicator::run(argv[i + 1]);
 		}
 		else {
 			Communicator::run();
